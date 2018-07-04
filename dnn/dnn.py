168f9e1ae9b14c77b462584af7108d1fff16babe
satyavratwagle <satya.wagle@gmail.com>
2018-07-03 18:57:49 -0700

    Summer 2018 research

--- dnn/dnn.py
+++ dnn/dnn.py
@@ -12,12 +12,31 @@ import glob, os
 import numpy as np
 from sklearn import datasets
 
+#User defined parameters
+
+batch_size = 20
+
+iterations = 5000
+
+hidden_layer1_nodes = 2
+hidden_layer2_nodes = 2
+hidden_layer3_nodes = 2
+hidden_layer4_nodes = 2
+
+learning_rate = 0.0005
+
+label_size = 8
+
+datatype = tf.float16
+
+# Function definitions
+
 def normalize_cols(m):
     col_max = m.max(axis=0)
     col_min = m.min(axis=0)
     return ((m-col_min) / (col_max - col_min))-0.5
 
-path = "../git_repo/chunked"
+path = "chunked"
 magdata_csv = glob.glob(path + "/*.csv")
 
 mag_data = []
@@ -28,27 +47,33 @@ for file in magdata_csv:
         for row in reader:
             if len(row)>0:
                 file_data.append(round(float(row[0]),4))
-        if max(file_data)>10:
-            mag_data.append(file_data)
+        #if max(file_data)>10:
+         #   mag_data.append(file_data)
+        mag_data.append(file_data)
 
 # mag_data is the entirety of training data
 # [-2] is speed
     
 training_data = []
-labels = []    
+labels = []
+speed_labels = []
+vehicle_labels = []
+    
 for sample in mag_data:
     training_data.append(sample[:-3])
-    labels.append(sample[-2])
+    speed_labels.append(sample[-2])
+    vehicle_labels.append(sample[-3])
     
 # Sample size 1200 --> Shape = (1200,)
     
 # convert to numpy arrays
 training_data = np.asarray(training_data)
+training_data = training_data.astype(np.float32)
 
-raw_labels = labels
+raw_labels = vehicle_labels
 new_labels = []
 for label in raw_labels:
-    temp_label = np.zeros(5)
+    temp_label = np.zeros(label_size)
     index = int(label)
     temp_label[index] = 1
     new_labels.append(temp_label)
@@ -77,31 +102,25 @@ sess = tf.Session()
 
 # Declare batch size and placeholders
 
-batch_size = 50
-x_data = tf.placeholder(shape=[None,1200], dtype=tf.float32)
-y_target = tf.placeholder(shape=[5,None,1], dtype=tf.float32)
+x_data = tf.placeholder(shape=[None,1200], dtype=datatype)
+y_target = tf.placeholder(shape=[label_size,None,1], dtype=datatype)
 
 # Architecture of Network --> get weights here
 
-hidden_layer1_nodes = 100
-hidden_layer2_nodes = 80
-hidden_layer3_nodes = 50
-hidden_layer4_nodes = 20
+A1 = tf.Variable(tf.random_normal(shape=[1200,hidden_layer1_nodes],dtype=datatype))
+b1 = tf.Variable(tf.random_normal(shape=[hidden_layer1_nodes],dtype=datatype))
 
-A1 = tf.Variable(tf.random_normal(shape=[1200,hidden_layer1_nodes]))
-b1 = tf.Variable(tf.random_normal(shape=[hidden_layer1_nodes]))
+A2 = tf.Variable(tf.random_normal(shape=[hidden_layer1_nodes,hidden_layer2_nodes],dtype=datatype))
+b2 = tf.Variable(tf.random_normal(shape=[hidden_layer2_nodes],dtype=datatype))
 
-A2 = tf.Variable(tf.random_normal(shape=[hidden_layer1_nodes,hidden_layer2_nodes]))
-b2 = tf.Variable(tf.random_normal(shape=[hidden_layer2_nodes]))
+A3 = tf.Variable(tf.random_normal(shape=[hidden_layer2_nodes,hidden_layer3_nodes],dtype=datatype))
+b3 = tf.Variable(tf.random_normal(shape=[hidden_layer3_nodes],dtype=datatype))
 
-A3 = tf.Variable(tf.random_normal(shape=[hidden_layer2_nodes,hidden_layer3_nodes]))
-b3 = tf.Variable(tf.random_normal(shape=[hidden_layer3_nodes]))
+A4 = tf.Variable(tf.random_normal(shape=[hidden_layer3_nodes,hidden_layer4_nodes],dtype=datatype))
+b4 = tf.Variable(tf.random_normal(shape=[hidden_layer4_nodes],dtype=datatype))
 
-A4 = tf.Variable(tf.random_normal(shape=[hidden_layer3_nodes,hidden_layer4_nodes]))
-b4 = tf.Variable(tf.random_normal(shape=[hidden_layer4_nodes]))
-
-An = tf.Variable(tf.random_normal(shape=[hidden_layer4_nodes,5]))
-bn = tf.Variable(tf.random_normal(shape=[5]))
+An = tf.Variable(tf.random_normal(shape=[hidden_layer4_nodes,label_size],dtype=datatype))
+bn = tf.Variable(tf.random_normal(shape=[label_size],dtype=datatype))
 
 # Layer outputs 
 
@@ -115,18 +134,18 @@ hidden1_out =  tf.add(tf.matmul(x_data,A1),b1)
 hidden2_out =  tf.add(tf.matmul(hidden1_out,A2),b2)
 hidden3_out =  tf.add(tf.matmul(hidden2_out,A3),b3)
 hidden4_out = tf.add(tf.matmul(hidden3_out,A4),b4)
-final_out = tf.add(tf.matmul(hidden4_out,An),bn)
+final_out = tf.add(tf.matmul(hidden2_out,An),bn)
 
 # Define Loss function
 
 #loss = tf.reduce_mean(tf.square(y_target-final_out))
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=final_out, labels=tf.transpose(y_target)))
-
+#acc = tf.subtract(tf.transpose(y_target),tf.one_hot(tf.nn.top_k(final_out).indices, tf.shape(final_out)))
 
 # Optimizer function
 
 #my_opt = tf.train.GradientDescentOptimizer(0.0005)
-my_opt = tf.train.AdamOptimizer(0.005)
+my_opt = tf.train.AdamOptimizer(learning_rate)
 
 train_step = my_opt.minimize(loss)
 
@@ -143,7 +162,7 @@ cum_loss = 0
 
 # Train 
 
-for i in range(5000):
+for i in range(iterations):
     
     # Get random batch
     
@@ -163,19 +182,23 @@ for i in range(5000):
     test_temp_loss = sess.run(loss, feed_dict={x_data : mag_data_test, y_target:np.transpose([labels_test])})
     test_loss.append(np.sqrt(test_temp_loss))
     
+    #print(temp_acc)
+    
+    cum_loss = 0
     cum_loss+=temp_loss
     # Print updates
     if (i+1)%100==0:
         cum_loss = cum_loss/100
+        test_loss.append(np.sqrt(cum_loss))
         print('Generation: ' + str(i+1) + '. Loss = ' + str(cum_loss))
-        cum_loss = 0
-        
-# Plot values
-        
+   
+# Plot values   
 plt.plot(loss_vec, 'k-', label='Train Loss')
 #plt.plot(test_loss, 'r--', label='Test Loss')
-plt.title('Loss (MSE) per Generation')
+plt.title('Final Loss : '+str(cum_loss))
 plt.xlabel('Generation')
 plt.ylabel('Loss')
 plt.legend(loc='upper right')
+#plt.savefig('../mag_plots/events/'+str(sel_vehicle)+'_'+str(sel_speed)+'_'+str(sel_sensor)+'.png')
 plt.show()
+sess.close()
---
+++ vanilla_nn.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 23:01:49 2018
+
+@author: satya
+"""
+# Import dependencies
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+import csv
+ 
+samplen = 9000
+batch_size = 1000
+generations = 100000
+
+sample_length = 4800
+
+# Architecture of Network --> get weights here
+
+hidden_layer_nodes = 200
+hidden_layer_nodes_2 = 150
+hidden_layer_nodes_3 = 100
+hidden_layer_nodes_4 = 75
+hidden_layer_nodes_5 = 50
+hidden_layer_nodes_6 = 20
+hidden_layer_nodes_7 = 10
+
+hidden_layer_nodes_f = 5
+
+step_size = 0.005
+
+#--------------------------------------------------------------------------------------------------------------
+
+# Load data from csv
+mag_dataset = []
+choice = []
+
+with open('training_data.csv', 'rU') as f:  #opens PW file
+    reader = csv.reader(f)
+    mag_data = list(list(rec) for rec in csv.reader(f, delimiter=',')) #reads csv into a list of lists
+
+
+x_vals = np.array([x[0:sample_length] for x in mag_data])
+
+'''choice = np.random.choice(len(x_vals), size = samplen)
+x_vals = x_vals[np.array(choice)]'''
+
+x_vals = x_vals.astype(np.float)
+x_vals = tf.keras.utils.normalize(x_vals,axis=-1,order=2)
+
+train_indices = np.random.choice(len(x_vals), round(len(x_vals)*0.8), replace=False)
+rem = np.array(list(set(range(len(x_vals))) - set(train_indices)))
+validation_indices = np.random.choice(len(rem), round(len(rem)*0.5), replace=False)
+test_indices = np.array(list(set(range(len(rem))) - set(validation_indices)))
+
+#y_vals = np.array([x[4800:4805] for x in mag_data]) # 4800:4805 for speed ; 4805:4813 for type if One hot encoded
+
+y_vals = np.array([x[sample_length] for x in mag_data])
+#y_vals = np.array(y_vals[np.array(choice)])
+y_vals = y_vals.astype(np.float)
+y_vals = y_vals.astype(np.int32)
+
+sess = tf.Session()
+
+# Fix seed
+seed = 2
+tf.set_random_seed(seed)
+np.random.seed(seed)
+
+# 80:10:10 data split and normalization
+
+x_vals_train = x_vals[train_indices]
+x_vals_validation = x_vals[validation_indices]
+x_vals_test = x_vals[test_indices]
+
+y_vals_train = y_vals[train_indices]
+y_vals_validation = y_vals[validation_indices]
+y_vals_test = y_vals[test_indices]
+
+y_vals_train = np.array(y_vals_train)
+y_vals_validation = np.array(y_vals_validation)
+y_vals_test = np.array(y_vals_test)
+
+def normalize_cols(m):
+    col_max = m.max(axis=0)
+    col_min = m.min(axis=0)
+    return (m-col_min) / (col_max - col_min)
+
+#x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
+#x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
+#x_vals_train = np.nan_to_num(x_vals_train)
+#x_vals_test = np.nan_to_num(x_vals_test)
+
+# Declare batch size and placeholders
+
+x_data = tf.placeholder(shape=[None, sample_length], dtype=tf.float32)
+y_target = tf.placeholder(shape=[None], dtype=tf.int32)
+
+#*****************************************************************************
+# Change functions from here on out if architecture changes
+
+'''filterz = tf.random_normal(shape=[3,1,1], dtype=tf.float32)
+filterz = tf.expand_dims(filterz, axis = 2)
+#conv2 = tf.nn.conv1d(x_data,filterz, stride=2, padding="VALID")
+conv2 = tf.layers.conv1d(inputs=x_data,filters=1,kernel_size=3,strides=2,use_bias='True',padding="VALID")
+dense = tf.layers.dense(inputs=conv2, units=1024, activation=tf.nn.relu)
+A1 = tf.Variable(tf.random_normal(shape=[conv2,hidden_layer_nodes]))'''
+
+A1 = tf.Variable(tf.random_normal(shape=[sample_length,hidden_layer_nodes]))
+b1 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes]))
+
+A2 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes,hidden_layer_nodes_2]))
+b2 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_2]))
+
+A3 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_2,hidden_layer_nodes_3]))
+b3 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_3]))
+
+A4 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_3,hidden_layer_nodes_4]))
+b4 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_4]))
+
+A5 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_4,hidden_layer_nodes_5]))
+b5 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_5]))
+
+A6 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_5,hidden_layer_nodes_6]))
+b6 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_6]))
+
+A7 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_6,hidden_layer_nodes_7]))
+b7 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_7]))
+
+Af = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_7,hidden_layer_nodes_f]))
+bf = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes_f]))
+
+# Layer outputs 
+
+hidden_out = tf.nn.sigmoid(tf.add(tf.matmul(x_data,A1),b1))
+hidden_out_2 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out,A2),b2))
+hidden_out_3 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out_2,A3),b3))
+hidden_out_4 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out_3,A4),b4))
+hidden_out_5 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out_4,A5),b5))
+hidden_out_6 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out_5,A6),b6))
+hidden_out_7 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out_6,A7),b7))
+final_out = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out_7,Af),bf))
+fprob = tf.nn.softmax(final_out, name=None)
+
+
+#*****************************************************************************
+
+# Define Loss function
+
+loss = tf.losses.sparse_softmax_cross_entropy(labels=y_target,logits=final_out)
+#loss = tf.losses.softmax_cross_entropy(onehot_labels=y_target,logits=final_out)
+
+# Optimizer function
+
+my_opt = tf.train.AdagradOptimizer(step_size)
+#my_opt = tf.train.AdagradOptimizer(step_size)
+train_step = my_opt.minimize(loss)
+
+# Initialize all variables
+
+init = tf.initialize_all_variables()
+sess.run(init)
+
+# Log vectors
+
+loss_vec = []
+test_loss = []
+test_pred = []
+pred_vec = []
+success_rate = []
+successful_guesses = []
+# Train 
+
+for i in range(generations):
+    
+    # Get random batch
+    
+    rand_index = np.random.choice(len(x_vals_train), size = batch_size)
+    rand_x = x_vals_train[rand_index]
+    rand_y = y_vals_train[rand_index]
+    
+    # Training step
+    
+    sess.run(train_step, feed_dict={x_data : rand_x, y_target:rand_y})
+    temp_loss,get_pred = sess.run([loss,fprob], feed_dict={x_data : rand_x, y_target:rand_y} )
+    loss_vec.append(temp_loss)
+    guess = np.argmax(get_pred,axis=1)
+    
+    pred_vec.append(guess)
+    correct_pred = np.sum(np.equal(guess,rand_y))
+    successful_guesses.append(correct_pred)
+    success_rate.append(correct_pred/batch_size)
+    
+    # Get testing loss
+    
+    test_temp_loss,predict = sess.run([loss,fprob], feed_dict={x_data : x_vals_test, y_target:y_vals_test})
+    test_loss.append(test_temp_loss)
+    test_pred.append(predict)
+    test_guess = np.argmax(predict,axis=1)
+    test_correct_pred = np.sum(np.equal(test_guess,y_vals_test))
+    test_accuracy = round(test_correct_pred*100/len(y_vals_test),4)
+    
+    # Print updates
+    if (i+1)%100==0:
+        print('Generation: ' + str(i+1) + '. Loss = ' + str((temp_loss))+'. Test Loss = ' + str((test_temp_loss))+'. Test Accuracy = ' + str((test_accuracy)))
+        #print('Generation: ' + str(i+1) + '. Loss = ' + str((temp_loss))+". Accuracy "+str(correct_pred*100/batch_size)+"%")
+        
+# Plot values
+        
+plt.plot(loss_vec, 'k-', label='Train Loss')
+plt.plot(test_loss, 'r--', label='Test Loss')
+plt.title('Loss (MSE) per Generation')
+plt.xlabel('Generation')
+plt.ylabel('Loss')
+plt.legend(loc='upper right')
+plt.show()
+
+plt.plot(success_rate, 'r--', label='Test Loss')
+plt.title('Accuracy')
+plt.xlabel('Generation')
+plt.ylabel('Success Rate')
+plt.legend(loc='upper right')
+plt.show()
